05b4f7e23d7d5288bacc28592458c0a2e901fd9e
shok <you@example.com>
2018-06-01 21:30:04 -0300

    Emprolijamos el codigo del emisor
    configuramos puerto de entrada, y se verificaron los pines de entrada.
    La etapa de captacion de datos esta funcionando.

--- codigoControlSalida/codigoControlSalida.ino
+++ codigoControlSalida/codigoControlSalida.ino
@@ -6,15 +6,12 @@
 
   This example code is in the public domain.
 */
-/*
-int pushButton2 = 2;
-int pushButton3 = 3;
-int pushButton4 = 4;
-*/
+
 /* the setup routine runs once when you press reset:*/
 #define MAX_DATOS 11
-#define HEADER_1 0xFF
-#define HEADER_2 0x0A
+#define HEADER_1 0x01
+#define HEADER_2 0x03
+
   
   uint8_t RX;
   uint8_t RY;
@@ -27,42 +24,44 @@ int pushButton4 = 4;
  
   uint8_t data[MAX_DATOS];
 
+  int ok = 0;
+  
 void setup() {
   // initialize serial communication at 9600 bits per second:
 
-
   Serial.begin(9600);
-
   
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   
-  RX = (uint8_t)analogRead(A0);
-  RY = (uint8_t)analogRead(A1);;
-  LX = (uint8_t)analogRead(A3);
-  LY = (uint8_t)analogRead(A2);
-  CH6 = 0;//(uint8_t)analogRead (A4);
-  TRAINER = 0;//(uint8_t)digitalRead(D1);//ver si va con D antes del 2
-  CH5 = 0;//(uint8_t)digitalRead(D2);
-  ELEV = 0;//(uint8_t)digitalRead(D3);
- 
+  LX = (uint8_t)analogRead(A0);
+  LY = (uint8_t)analogRead(A1);;
+  RY = (uint8_t)analogRead(A2);
+  RX = (uint8_t)analogRead(A3);
+  CH6 = (uint8_t)analogRead (A4);
+  TRAINER = (uint8_t)digitalRead(2);//ver si va con D antes del 2
+  CH5 = (uint8_t)digitalRead(3);
+  ELEV = (uint8_t)digitalRead(4);
+                                                                             
   // read the input on analog pin 0:
   data[0] = HEADER_1;
-  data[1] = HEADER_1;//HEADER_2;
-  data[2] =HEADER_1;// RX;
-  data[3] = HEADER_1;//RY;
-  data[4] = HEADER_1;//LX;
-  data[5] = HEADER_1;//LY;
-  data[6] = HEADER_1;//CH6;
-  data[7] = HEADER_1;//TRAINER;
-  data[8] = HEADER_1;//CH5;
-  data[9] = HEADER_1;//ELEV;
+  data[1] = HEADER_2;
+  data[2] = RX;
+  data[3] = RY;
+  data[4] = LX;
+  data[5] = LY; 
+  data[6] = CH6;
+  data[7] = TRAINER;
+  data[8] = CH5;
+  data[9] = ELEV;
   data[10] = XORChecksum8(data,MAX_DATOS-1);//mando menos uno porque el check sum es el dato 10
 
-  
-  delay(1000);//para probar el dato en pantalla
+  //Serial.print(data[10]);
+
+
+  delay(300);//para probar el dato en pantalla
 }
  
 uint8_t XORChecksum8(byte *data, size_t dataLength)
@@ -73,26 +72,12 @@ uint8_t XORChecksum8(byte *data, size_t dataLength)
     for (size_t i = 0; i < dataLength ; i++)
     {
        value ^= (uint8_t)data[i];
-       Serial.print(value);
+       //Serial.print(value);
+       //delay(200);
     }
     return value;
 }
-
-
-/*void itoc(
-{
-int a=1;
-
-char b[2];
-
-String str;
-
-str=String(a);
-
-str.toCharArray(b,2);
-}*/
- 
- /* // print out the value you read:
+  /*  // print out the value you read:
   Serial.print(RX);
   Serial.print('\t');
   Serial.print(RY);
@@ -109,5 +94,21 @@ str.toCharArray(b,2);
   Serial.print('\t');
   Serial.print(ELEV);
   Serial.print('\n');
-  delay(1);     // delay in between reads for stability
+  delay(1);     // delay in between reads for stability*/
+  
+
+/*void itoc(
+{
+int a=1;
+
+char b[2];
+
+String str;
+
+str=String(a);
+
+str.toCharArray(b,2);
 }*/
+ 
+
+
